3e9ea88a7ae4bd0539ffb5ffa8c09cf6efb77ce2
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-01-10 20:33:48 +0100

    Connecting to DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import  express  from "express";
+import {config} from "dotenv";
 
-
+config()
 const app = express()
 
+//Middlewares
 app.use(express.json())
 
 
-
+//Connections and Listeners
 app.listen(5000, () => console.log("Server Open"))
