fd6da14eae12f07974a37eeb37ca1b868d2148d4
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-01-24 21:25:55 +0100

    Login 70% ready

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -5,9 +5,32 @@ const Login = () => {
     return (
         <Box width={"100%"} height={"100%"} display={"flex"} flex={1}> 
             <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none"}}>
-                <img src="airbot.png" alt='Robot' style={{width: "400px"}}/>
+                <img src="airobot.png" alt='Robot' style={{width: "400px"}}/>
             </Box>
-            <Box></Box>
+            <Box
+                display={"flex"}
+                flex={{ xs: 1, md: 0.5}}
+                justifyContent={"center"}
+                alignItems={"center"}
+                padding={2}
+                ml={"auto"}
+                mt={16}
+                borderColor={"red"}
+            >
+                <form
+                    style={{
+                        margin: "auto",
+                        padding: "30px",
+                        boxShadow: "10px 10px 20px #000",
+                        borderRadius: "10px",
+                        border: "none",
+                    }}
+                >
+                    <Box sx={{display:"flex", flexDirection:"column", justifyContent: "center"}}>
+                        
+                    </Box>
+                </form>
+            </Box>
         </Box>
     )
 }
